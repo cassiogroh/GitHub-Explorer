6b6dc58ca36c72f09a51c6b081b203ac0d173473
cassiogroh <cassio_groh@hotmail.com>
2020-09-02 09:03:20 -0300

    Updates

---
+++ precache-manifest.b2a677fd1485e0497a386984f3ef9fbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8b1cf2ad7eb8f7709f79510d2c8722e",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "d6871abaaec1498936e2",
+    "url": "/my-app/static/js/2.e9562538.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.e9562538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c735f6f24fbff08387e",
+    "url": "/my-app/static/js/main.d1be5b7a.chunk.js"
+  },
+  {
+    "revision": "98a35cfded88a39fce9b",
+    "url": "/my-app/static/js/runtime-main.9e448db9.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/my-app/static/media/github-background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/my-app/static/media/logo.04f6798f.svg"
+  }
+]);
